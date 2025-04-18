4ac12f2bc623b7379a75bed4a5e38913bc03691b
Denis Ndiritu <denisndiritu1@gmail.com>
2025-04-18 23:15:10 +0300

    Created a program that prints prices of itsms in a shop

---
+++ src/computationalThinking.kt
@@ -0,0 +1,6 @@
+fun main(){
+//    Computational thinking - a set of skills that help you come up
+//    with a generalizable solution to a problem and solve it using a computer.
+//    Foundations of these skills: abstraction, decomposition, pattern recognition, and evaluation
+//    Decomposition: Breaking down a complex problem ino separate steps and subproblems.
+}
---
+++ src/printThePrices.kt
@@ -0,0 +1,27 @@
+//Description
+//You've recently opened a new corner shop. It is relatively small; it contains only bubblegum, toffee, ice cream, milk chocolate, doughnuts, and pancakes. The first version of the program will display a list of all the products with their prices.
+//
+//Objectives
+//Print the Prices: line;
+//
+//Print the item names and prices from the Example below. Your output format should follow it.
+//
+//Example
+//Example 1: output at the end of this stage
+//
+//Prices:
+//Bubblegum: $2
+//Toffee: $0.2
+//Ice cream: $5
+//Milk chocolate: $4
+//Doughnut: $2.5
+//Pancake: $3.2
+fun main(){
+    println("Prices:")
+    println("Bubblegum: $2")
+    println("Toffee: $0.2")
+    println("Ice cream: $5")
+    println("Milk chocolate: $4")
+    println("Doughnut: $2.5")
+    println("Pancake: $3.2")
+}
